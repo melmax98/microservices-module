fb4cc11e5628945229ee5080b47c443a79e234a2
Maksym Melnyk <Maksym_Melnyk2@epam.com>
2021-11-11 17:08:36 +0200

    Initial commit

---
+++ src/main/java/com/example/microservicesmodule/MicroservicesModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicesmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesModuleApplication.class, args);
+    }
+
+}
